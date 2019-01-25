fe836f40c2f6351c2c79abb236f28476322cb60b
mjacks258 <matthew_jackson@comcast.com>
2019-01-25 13:43:21 -0700

    added licensing header and fixed cachegroup.go for broken test

--- traffic_portal/app/src/common/directives/dragAndDrop/dragdropDirective.js
+++ traffic_portal/app/src/common/directives/dragAndDrop/dragdropDirective.js
@@ -1,18 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 var DndEnable = function($compile) {
     return {
-        restrict: 'A',  // use as an Attribute only
+        restrict: 'A',
         replace: false,
-        terminal: true, //this setting is crucial since I'm adding attributes that need to be compiled
-        priority: 1050, // "me first", ng-repeat priority is 1000
+        terminal: true,
+        priority: 1050,
         link: function(scope, element, attrs) {
-            if (attrs.dndEnable) { // if dnd-enable evaluates to true
+            if (attrs.dndEnable) {
                 if (attrs.pageid === 'cacheGroupFallback') {
                     element.attr('dragsmart', 'handleDrag(fb)');
                     element.attr('droppable', 'true');
                     element.attr('drop', 'handleDrop(fb)');
                 }
             }
-            element.removeAttr('dnd-enable'); // prevent infinite loop on compile
+            element.removeAttr('dnd-enable');
             $compile(element)(scope);
         }
     };
--- traffic_portal/app/src/common/directives/dragAndDrop/droppableDirective.js
+++ traffic_portal/app/src/common/directives/dragAndDrop/droppableDirective.js
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 var DroppableDirective = function () {
     return {
         scope: {
@@ -12,7 +31,6 @@ var DroppableDirective = function () {
                 'dragover',
                 function(e) {
                     e.dataTransfer.dropEffect = 'move';
-                    // allows us to drop
                     if (e.preventDefault) e.preventDefault();
                     this.classList.add('over');
                     var rect = e.target.getBoundingClientRect();
@@ -71,7 +89,7 @@ var DroppableDirective = function () {
                 'drop',
                 function(e) {
                     if (e.stopPropagation) e.stopPropagation();
-                    if (e.preventDefault) e.preventDefault(); // *needed* for firefox
+                    if (e.preventDefault) e.preventDefault();
                     this.classList.remove('over');
                     if (e.target.classList.contains('drop-child')) {
                         e.target.parentElement.style.borderTop = '';
@@ -81,7 +99,7 @@ var DroppableDirective = function () {
                         e.target.style.borderBottom = '';
                     }
                     var from = e.dataTransfer.getData("Text");
-                    if (from != this.textContent) { // ignore drop on self
+                    if (from != this.textContent) {
                         scope.$apply(function(scope) {
                             var fn = scope.drop();
                             if ('undefined' !== typeof fn) {
@@ -101,7 +119,7 @@ var DroppableDirective = function () {
                 function (e) {
                     if (e.stopPropagation) e.stopPropagation();
                     e.dataTransfer.effectAllowed = 'move';
-                    e.dataTransfer.setData('Text',this.textContent);  // some value *required* for firefox; "Text" - not "text/plain" for IE
+                    e.dataTransfer.setData('Text',this.textContent);
                     this.classList.add('drag');
                     scope.$apply(function (scope) {
                         var fn = scope.dragsmart();
